770a2915e3209372a3082c84c3568a4ce27e54b1
phzeng <ph.zeng0726@gmail.com>
2022-07-05 17:50:55 +0800

    chore: add intl dependencies in pubspec.yaml

--- chat_app_demo/lib/main.dart
+++ chat_app_demo/lib/main.dart
@@ -9,10 +9,10 @@ import 'presentation/core/app_widgets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // // NOTE:
-  // // getId && injectable 教學，主要是用在
-  // // firestore_helpers.dart 跟 firebase_injectable_module.dart
-  // // https://juejin.cn/post/7060525200139354148
+  // NOTE:
+  // getId && injectable 教學，主要是用在
+  // firestore_helpers.dart 跟 firebase_injectable_module.dart
+  // https://juejin.cn/post/7060525200139354148
   configureInjection(Environment.prod); // 沒有這行的話沒辦法用injection
   getIt.registerSingleton<RootRouter>(RootRouter());
   await Firebase.initializeApp(
